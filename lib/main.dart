d1b71040c1b84e0dc2e14e9ab0b7853f539bd3a3
Dudu <anirudhrajagopalv@gmail.com>
2025-02-28 02:14:09 +0530

    Updated comments

--- lib/main.dart
+++ lib/main.dart
@@ -4,21 +4,27 @@ import 'dart:math';
 // Main application 
 void main() {
   return runApp(
+    // Material App is being created
     MaterialApp(
       home: SafeArea(
         child: Scaffold(
+          // Background color has been changed to blue
           backgroundColor: Colors.blue[200],
+          //AppBar has been created
           appBar: AppBar(
+            //Background of the AppBar has been set to blue
             backgroundColor: Colors.blue[800],
             title: Center(
               child: Text(
                 // Main header text
                 'Ask Me Anything',
                 style:
+                // Text style has been added and the color of the text is changed to white with a bold font
                     TextStyle(color: Colors.white, fontWeight: FontWeight.bold),
               ),
             ),
           ),
+          //Body of the App 
           body: MagicBall(),
         ),
       ),
@@ -26,6 +32,7 @@ void main() {
   );
 }
 // Stateful widget for the magic ball functionality
+// Allows the widget to maintain state that can change depending on the circumstances
 class MagicBall extends StatefulWidget {
   const MagicBall({super.key});
 
@@ -38,15 +45,19 @@ class _MagicBallState extends State<MagicBall> {
   int ball = 1;
   @override
   Widget build(BuildContext context) {
+    // Gesture Detector has been created to map all the taps
     return GestureDetector(
         onTap: () {
+          //SetState triggers the rebuild of the app with new predictions
           setState(() { 
+            //Generating random numbers between 1 to 5 
             ball = Random().nextInt(5) + 1;
           });
         },
         child: Padding(
           padding: const EdgeInsets.all(16.0),
           child: Center(
+            // Row widget is being used to arrange the 8 ball images horizontally 
             child: Row(
               children: [
                 Expanded(
